41b6359ae30f308b644210b16ccc1f5e400a34ea
manivannan-robot <manivannan@dev-bee.com>
2023-05-20 14:45:04 +0530

    initial commit when installed freshly

---
+++ resources/views/website/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background: url('background.jpg'); /* Add your own background image */
+            background-size: cover;
+            padding: 50px;
+            font-family: Arial, sans-serif;
+        }
+
+        .glass {
+            background: rgba(255, 255, 255, 0.1);
+            border-radius: 10px;
+            padding: 30px;
+            max-width: 500px;
+            margin: auto;
+            backdrop-filter: blur(10px);
+            box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
+        }
+
+        h1, h2, p {
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <div class="glass">
+        <h1>John Doe</h1>
+        <h2>Web Developer</h2>
+        <p><strong>Email:</strong> john.doe@example.com</p>
+        <p><strong>Skills:</strong> HTML, CSS, JavaScript, PHP, Laravel</p>
+        <p><strong>Experience:</strong> 5 years</p>
+        <p><strong>About:</strong> Passionate web developer with a knack for creating elegant solutions in the least amount of time.</p>
+    </div>
+</body>
+</html>
